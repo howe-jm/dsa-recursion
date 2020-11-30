2962b3376dae12f2731f04438599989b5ada02a8
Jason Howe <trisynth@gmail.com>
2020-11-30 18:36:17 -0500

    DSA Recursion Assignment

--- dsa-recursion.js
+++ dsa-recursion.js
@@ -44,6 +44,20 @@ console.log(stringReverser('This is a test string'));
 
 //4. nth Triangular Number
 
+function nthTriangularNumber(num1, count = 1, seq = []) {
+  if (num1 === 1) {
+    return '1';
+  }
+  if (num1 === count) {
+    return seq.reduce((a, b) => a + b, 0);
+  }
+  seq.push(count);
+  count++;
+  return nthTriangularNumber(num1, count, seq);
+}
+
+console.log(nthTriangularNumber(10));
+
 //5. String Splitter
 
 function stringSplitter(string, split = '', array = []) {
@@ -65,6 +79,23 @@ function stringSplitter(string, split = '', array = []) {
 console.log(stringSplitter('02/20/2020'));
 
 //6. Fibonacci
+
+function fibonacci(num1, count = 2, seq = [1, 1]) {
+  if (num1 === 1) {
+    return '1';
+  }
+  if (num1 === count) {
+    return seq.join(', ');
+  }
+
+  let newNum = seq[count - 1] + seq[count - 2];
+  seq.push(newNum);
+  count++;
+
+  return fibonacci(num1, count, seq);
+}
+
+console.log(fibonacci(7));
 
 //7. Factorial
 
